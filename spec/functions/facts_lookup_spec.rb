c9cc7b547f07cab669ea7bba59f21bb57056ca4f
Sean Millichamp <sean@bruenor.org>
2017-01-27 11:40:44 -0500

    Add facts support to function testing

---
+++ spec/functions/facts_lookup_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec-puppet'
+
+describe 'structured_facts::lookup' do
+  context 'with one set of values' do
+    let(:facts) {{ 'os' => {'family' => 'RedHat'} }}
+
+    it { is_expected.to run.and_return('RedHat') }
+  end
+
+  context 'with a different set of values' do
+    let(:facts) {{ 'os' => {'family' => 'Suse'} }}
+
+    it { is_expected.to run.and_return('Suse') }
+  end
+end
